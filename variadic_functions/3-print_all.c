e6ae7e5771d7c7a9b125017d1092e9faa38a92dc
Wasaal-tech <wesaalebraheem@hotmail.com>
2026-04-12 15:16:22 +0000

    Fix task 3 print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,11 +10,12 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i;
+	unsigned int i;
 	int sep;
 	char *str;
-	char *types = "cifs";
 	char c;
+	double f;
+	int n;
 
 	va_start(args, format);
 	i = 0;
@@ -23,26 +24,40 @@ void print_all(const char * const format, ...)
 	{
 		c = format[i];
 		str = NULL;
-		if (c == 'c' || c == 'i' || c == 'f' || c == 's')
+		if (c == 'c')
 		{
+			n = va_arg(args, int);
 			if (sep)
 				printf(", ");
-			if (c == 'c')
-				printf("%c", va_arg(args, int));
-			else if (c == 'i')
-				printf("%d", va_arg(args, int));
-			else if (c == 'f')
-				printf("%f", va_arg(args, double));
-			else
-			{
-				str = va_arg(args, char *);
-				printf("%s", str ? str : "(nil)");
-			}
+			printf("%c", n);
+			sep = 1;
+		}
+		if (c == 'i')
+		{
+			n = va_arg(args, int);
+			if (sep)
+				printf(", ");
+			printf("%d", n);
+			sep = 1;
+		}
+		if (c == 'f')
+		{
+			f = va_arg(args, double);
+			if (sep)
+				printf(", ");
+			printf("%f", f);
+			sep = 1;
+		}
+		if (c == 's')
+		{
+			str = va_arg(args, char *);
+			if (sep)
+				printf(", ");
+			printf("%s", str ? str : "(nil)");
 			sep = 1;
 		}
 		i++;
 	}
-	(void)types;
 	va_end(args);
 	printf("\n");
 }
