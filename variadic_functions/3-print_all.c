258c35845381b87699393d8794fb0af718a05261
Wasaal-tech <wesaalebraheem@hotmail.com>
2026-04-12 15:36:18 +0000

    Fix task 3 Betty style

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,6 +2,53 @@
 #include <stdio.h>
 
 /**
+ * print_char - prints a char
+ * @args: va_list
+ *
+ * Return: void
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an int
+ * @args: va_list
+ *
+ * Return: void
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ * @args: va_list
+ *
+ * Return: void
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints a string
+ * @args: va_list
+ *
+ * Return: void
+ */
+void print_string(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	printf("%s", str ? str : "(nil)");
+}
+
+/**
  * print_all - prints anything
  * @format: list of types of arguments
  *
@@ -12,9 +59,14 @@ void print_all(const char * const format, ...)
 	va_list args;
 	unsigned int i;
 	int sep;
-	char *str;
 	int j;
-	char types[] = "cifs";
+	printer_t types[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{0, NULL}
+	};
 
 	va_start(args, format);
 	i = 0;
@@ -22,23 +74,13 @@ void print_all(const char * const format, ...)
 	while (format && format[i])
 	{
 		j = 0;
-		while (types[j] && types[j] != format[i])
+		while (types[j].f && types[j].c != format[i])
 			j++;
-		if (types[j])
+		if (types[j].f)
 		{
 			if (sep)
 				printf(", ");
-			if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-			if (format[i] == 'i')
-				printf("%d", va_arg(args, int));
-			if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-			if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				printf("%s", str ? str : "(nil)");
-			}
+			types[j].f(args);
 			sep = 1;
 		}
 		i++;
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,9 +3,24 @@
 
 #include <stdarg.h>
 
-int sum_them_all(const unsigned int n, ...);
+/**
+ * struct printer - struct to hold type and print function
+ * @c: the type character
+ * @f: the function to print
+ */
+typedef struct printer
+{
+	char c;
+	void (*f)(va_list);
+} printer_t;
 
-#endif /* VARIADIC_FUNCTIONS_H */
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
+#endif /* VARIADIC_FUNCTIONS_H */
