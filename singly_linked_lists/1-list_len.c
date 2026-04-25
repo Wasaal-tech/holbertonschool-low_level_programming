3e12adbb979e7f14d4e22cb6872e42ae83272ca4
Wasaal-tech <wesaalebraheem@hotmail.com>
2026-04-25 16:21:47 +0000

    Add task 1 list_len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a list
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
