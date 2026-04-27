cc5801f45a7bcc21f56482bfb814af4ce51f27a9
Wasaal-tech <wesaalebraheem@hotmail.com>
2026-04-27 14:38:06 +0000

    Fix task 3 hash_table_set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,11 +13,23 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
+	hash_node_t *tmp;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (tmp->value != NULL);
+		}
+		tmp = tmp->next;
+	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
