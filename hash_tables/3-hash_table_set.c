f56b105b21b7e7e7b1ecfb947207281d28dc55e7
Wasaal-tech <wesaalebraheem@hotmail.com>
2026-04-27 14:35:53 +0000

    Add task 3 hash_table_set

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value
+ *
+ * Return: 1 if succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || key[0] == '\0')
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -31,3 +31,4 @@ hash_table_t *hash_table_create(unsigned long int size);
 
 #endif /* HASH_TABLES_H */
 unsigned long int hash_djb2(const unsigned char *str);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
